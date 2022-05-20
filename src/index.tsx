af2585c5b9a7226513d817d3d4352baa3c8c8c76
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-05-19 21:54:08 -0300

    Adds the miragejs to the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createServer } from 'miragejs';
 
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Desenvolvimento de web site',
+          amount: 12000,
+          type: 'deposit',
+          category: 'Trabalho',
+          createdAt: '20/05/2022',
+        }
+      ]
+    });
+  }
+});
 
 const container = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(container);
